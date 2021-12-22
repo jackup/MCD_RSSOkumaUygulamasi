a21fcfeb3638d2f4e120835e2825c42e1058deb6
jackup <yk.58@hotmail.com>
2021-12-22 20:37:30 +0300

    Add project files.

---
+++ MCD_RSSOkumaUygulamasi/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace MCD_RSSOkumaUygulamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lstBaslik = new System.Windows.Forms.ListBox();
+            this.txtRSS1 = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IRSS Adres";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Haber Başlığı";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(884, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Haberin Detayı";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSubmit.Location = new System.Drawing.Point(715, 47);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(130, 29);
+            this.btnSubmit.TabIndex = 1;
+            this.btnSubmit.Text = "SUBMIT";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // lstBaslik
+            // 
+            this.lstBaslik.FormattingEnabled = true;
+            this.lstBaslik.ItemHeight = 16;
+            this.lstBaslik.Location = new System.Drawing.Point(49, 146);
+            this.lstBaslik.Name = "lstBaslik";
+            this.lstBaslik.Size = new System.Drawing.Size(796, 532);
+            this.lstBaslik.TabIndex = 2;
+            this.lstBaslik.SelectedIndexChanged += new System.EventHandler(this.lstBaslik_SelectedIndexChanged);
+            // 
+            // txtRSS1
+            // 
+            this.txtRSS1.Location = new System.Drawing.Point(49, 53);
+            this.txtRSS1.Name = "txtRSS1";
+            this.txtRSS1.Size = new System.Drawing.Size(637, 22);
+            this.txtRSS1.TabIndex = 3;
+            this.txtRSS1.Text = "https://www.sabah.com.tr/rss/ekonomi.xml";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(887, 92);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(747, 655);
+            this.webBrowser1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1662, 759);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.txtRSS1);
+            this.Controls.Add(this.lstBaslik);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.ListBox lstBaslik;
+        private System.Windows.Forms.TextBox txtRSS1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ MCD_RSSOkumaUygulamasi/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace MCD_RSSOkumaUygulamasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private List<Haber> XMLCevir()
+        {
+            List<Haber> haberKayitları = new List<Haber>();
+            XDocument xmlKaynak = XDocument.Load(txtRSS1.Text);
+            List<XElement> Rows = xmlKaynak.Descendants("item").ToList(); //item alt xml parçasını aldık
+            foreach (XElement item in Rows)
+            {
+                Haber temp = new Haber();
+                temp.baslik = item.Element("title").Value; //bunlar sabahın rss sitesinden aldık
+                temp.link = item.Element("link").Value;
+                temp.aciklama = item.Element("description").Value;
+                haberKayitları.Add(temp);
+            }
+            return haberKayitları;
+
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            List<Haber> kayitlar = XMLCevir();
+            lstBaslik.DataSource = kayitlar;
+        }
+
+        private void lstBaslik_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox secilenDeger = (ListBox)sender;
+            Haber secilenHaber = (Haber)secilenDeger.SelectedItem;
+            webBrowser1.DocumentText = secilenHaber.aciklama;
+        }
+    }
+}
